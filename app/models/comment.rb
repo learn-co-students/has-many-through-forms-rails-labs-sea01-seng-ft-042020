d02f184863deac03b5c525b71490ac10eebbaec9
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-05-18 21:35:21 -0700

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,12 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(user)
+    # Create a user if the username is not an empty field, if it is this will be skipped
+    if !user[:username].empty?
+      self.user = User.find_or_create_by(username: user[:username])
+      self.user.update(user)
+    end
+  end
+
 end
